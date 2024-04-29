172c644d23ce638e1a4b44229aa407d888a6cc1e
Akshay Pratap Singh <117713892+Akshayconqurers@users.noreply.github.com>
2024-04-29 09:36:25 +0530

    Create revreseprint.cpp

---
+++ revreseprint.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   int n;
+   cout<<"Enter the value of n : "<<endl;
+   cin>>n;
+   
+   for( int i=n; i >= 1; i--){
+       
+       cout<< i << " ";
+   }
+   
+   cout<<endl;
+
+  
+  
+  return 0;
+}
